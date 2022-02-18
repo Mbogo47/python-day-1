b1bc19aec1e2351e1b1ee227530b022a9cae4db0
Ivy <njokiivy47@gmail.com>
2022-02-18 16:35:25 +0300

    day 1 out of 100 created a band name generator

---
+++ learn.py
@@ -0,0 +1,30 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# print("Hello World!\nHello World!\nHello World!")
+# print("Hello" + " " + "Ivy")
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign')
+print('e.g. print("Hello" +"World")')
+print("New Lines can be created with a backslash and n.")
+
+# input() will get user input in console
+# Then print() will print the word "Hello" and the user input
+print("Hello " + input("What is your name?")+"!")
+input("What is your name? ")
+print(len(input("What is your name? ")))
+name = input("What is your name? ")
+print(len(name))
+name = "Jack"
+print(name)
+
+name = "Ivy"
+print(name)
+a= input("a:")
+b= input("b:")
+c = a
+a = b
+b = c
+print(a)
+print(b)
